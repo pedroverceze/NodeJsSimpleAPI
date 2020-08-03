ab301056f588b6d2d6af065164754fa7c972932a
pedroverceze <pedroverceze@hotmail.com>
2020-08-03 15:16:02 -0300

    lines removed

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 const express = require('express');
-
 const recipeController = require('./controllers/recipeController');
-
 
 const routes = express.Router();
 
-
-
 routes.get('/recipe', recipeController.index);
 routes.post('/recipe', recipeController.create);
-
-
 
 module.exports = routes;
